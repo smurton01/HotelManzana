eb6527b55552d80614aa87d7ac82249f0f2737dd
Saya Murton <saya.murton@gmail.com>
2017-09-21 12:30:30 -0600

    3 outlets & bar button item
    -added 3 new outlets to `AddRegistrationTableViewController` (firstNameTextField, lastNameTextField, emailTextField)
    -added a bar button item to right slot of `AddRegistrationTableViewController` & set it to the Done system item

--- HotelManzana/AddRegistrationTableViewController.swift
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -9,23 +9,19 @@
 import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
